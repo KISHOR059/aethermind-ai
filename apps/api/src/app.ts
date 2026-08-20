fe2bc75b5fe7b972548fa1118cb00ecfa20a3140
KISHOR059 <mtkishor07@gmail.com>
2026-08-20 17:16:09 +0530

    refactor: simplify helmet integration and update typescript configuration include paths

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,7 +2,7 @@ import compression from "compression";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
-import type { Express, RequestHandler } from "express";
+import type { Express } from "express";
 import helmet from "helmet";
 
 import { env } from "./config/env.js";
@@ -17,14 +17,8 @@ const app: Express = express();
 
 registerEventListeners();
 
-const helmetMiddleware = (
-  typeof helmet === "function"
-    ? helmet
-    : (helmet as unknown as { default: () => RequestHandler }).default
-) as () => RequestHandler;
-
 app.use(requestIdMiddleware);
-app.use(helmetMiddleware());
+app.use(helmet());
 app.use(
   cors({
     origin: (origin, callback) => {
